725c44a472cbf41bf92b1f664f44d2d534a0eee7
Anton Shchukin <a.a.shchukin@gmail.com>
2021-05-15 12:57:29 +0700

    compile

---
+++ production/scripts/_mfp.js
@@ -0,0 +1,9 @@
+(function($) {
+
+    $('.mfp-handler').magnificPopup({
+        type: 'inline',     /* to use custom HTML inside of popup */
+        removalDelay: 200,
+        showCloseBtn: false /* we will have our own cross button in .window component */
+    });
+
+})(jQuery);
