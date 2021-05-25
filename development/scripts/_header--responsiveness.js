13eebae48ee1d3a710e8917e8dd24fd2449085bd
Anton Shchukin <a.a.shchukin@gmail.com>
2021-05-25 09:56:25 +0700

    header functionality

---
+++ development/scripts/_header--responsiveness.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+    $('.header__search-handler, .header__profile-handler').on('click', function () {
+        $('.header').toggleClass('header--expanded');
+    });
+
+
+    /* click outside ( goo.gl/SJG2Hw ) */
+
+    $(document).on('click', function(event) {
+        if (!$(event.target).closest('.header__dropdown, .header__search-handler, .header__profile-handler').length) {
+            $('.header').removeClass('header--expanded');
+        }
+    });
+
+
+    /* Esc press */
+
+    $(document).on('keyup', function(event) {
+        if (event.keyCode === 27) {
+            $('.header').removeClass('header--expanded');
+        }
+    });
+});
