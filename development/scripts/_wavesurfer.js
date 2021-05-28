227f683f25954771e6b3c027badfb0ea0cd2dff3
Anton Shchukin <a.a.shchukin@gmail.com>
2021-05-28 09:06:14 +0700

    player

--- development/scripts/_wavesurfer.js
+++ development/scripts/_wavesurfer.js
@@ -3,18 +3,62 @@
     var wavesurfer = WaveSurfer.create({
         container: '.player__progress',
         barWidth: 1,
-        barHeight: 1, // the height of the wave
-        barGap: 2, // the optional spacing between bars of the wave, if not provided will be calculated in legacy format
+        barGap: 2,
+        height: '49',
         waveColor: '#FFFFFF',
         progressColor: '#BFD7E3',
-        cursorColor: 'red'
+        cursorColor: '#FFFFFF'
     });
 
-    wavesurfer.load('../temp/epic.mp3');
+    var current = 0;
+    var trackList = ['../temp/audio/epic.mp3', '../temp/audio/scream.mp3', '../temp/audio/reading.mp3'];
+
+    wavesurfer.load(trackList[current]);
 
     $('#temp-play').on('click', function (){
         wavesurfer.playPause();
     });
+
+
+
+    /* When song is loaded, decoded and the waveform add class .player--init */
+    wavesurfer.on('ready', function () {
+        $(wavesurfer.params.container).parents('.player').addClass('player--init')
+    });
+
+    /* Prev track */
+    $('#temp-backward').on('click', function () {
+        $(wavesurfer.params.container).parents('.player').removeClass('player--init')
+        if (current > 0) {
+            current--;
+        } else {
+            current = trackList.length - 1;
+        }
+        wavesurfer.load(trackList[current]);
+    });
+
+    /* Next track */
+    $('#temp-forward').on('click', function () {
+        $(wavesurfer.params.container).parents('.player').removeClass('player--init')
+        if (current < trackList.length - 1) {
+            current++;
+        } else {
+            current = 0;
+        }
+        wavesurfer.load(trackList[current]);
+    });
+
+
+    /* Toggle Play/Pause icon */
+    wavesurfer.on('play', function () {
+        var $playButton = $(wavesurfer.params.container).parents('.player').find('#temp-play')
+        $playButton.find('svg use').attr("xlink:href", "../symbols/symbols.svg#16-pause");
+    }).on('pause', function () {
+        var $playButton = $(wavesurfer.params.container).parents('.player').find('#temp-play')
+        $playButton.find('svg use').attr("xlink:href", "../symbols/symbols.svg#16-play-rounded");
+    });
+
+
 
 
 })(jQuery);
