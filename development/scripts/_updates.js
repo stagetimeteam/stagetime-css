ab0e47731c6d9af80e94cbfde1a76dfd650bd62a
Anton Shchukin <a.a.shchukin@gmail.com>
2021-06-01 12:51:59 +0700

    rewrite carousels to not to cut shadows

--- development/scripts/_updates.js
+++ development/scripts/_updates.js
@@ -1,7 +1,15 @@
 (function($) {
 
-    $('.updates__switch .toggler__widget').on('change', function (){
-        $('.updates').toggleClass('updates--switch-to-news')
-    });
+
+    function updatesSwitch() {
+        if ( $('.updates__switch .toggler__widget').prop('checked') ) {
+            $('.updates').addClass('updates--switch-to-news')
+        } else {
+            $('.updates').removeClass('updates--switch-to-news')
+        }
+    }
+
+    $('.updates__switch .toggler__widget').on('change', updatesSwitch);
+    $(document).ready(updatesSwitch);
 
 })(jQuery);
