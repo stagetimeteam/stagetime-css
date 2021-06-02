83edb8a6fbf7360886c37f87f840b8c5439a7935
Anton Shchukin <a.a.shchukin@gmail.com>
2021-06-02 12:36:43 +0700

    placeholder in select

--- development/scripts/_input.js
+++ development/scripts/_input.js
@@ -1,6 +1,14 @@
 (function($) {
 
+    /* Select placeholder */
 
+    $('.input--select .input__widget').on('change', function () {
+        if( $(this).val() === 'placeholder' ) {
+            $(this).parent('.input').addClass('input--placeholder-is-chosen')
+        } else {
+            $(this).parent('.input').removeClass('input--placeholder-is-chosen')
+        }
+    });
 
 
     /* Expand text area manually */
