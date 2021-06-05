c00b98048283bf6028892f20c206f52e298c54df
Anton Shchukin <a.a.shchukin@gmail.com>
2021-06-05 08:05:57 +0700

    compile

--- production/scripts/_mfp.js
+++ production/scripts/_mfp.js
@@ -6,9 +6,12 @@ $(document).ready(function () {
         showCloseBtn: false, /* we will have our own cross button in .window component */
         callbacks: {
             open: function() {
+                /* On popup open re-init Carousels: */
                 $('.mfp-content .carousel').each(function () {
                     initCarousel($(this));
                 });
+                /* re-init Expanding Textareas:: */
+                $('.edit-post__message--extendable-via-plugin').expanding();
             }
         }
     });
