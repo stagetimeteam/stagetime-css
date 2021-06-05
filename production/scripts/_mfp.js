93aadc20f58dd1f288e49fe10295f2ee7a8cdded
Anton Shchukin <a.a.shchukin@gmail.com>
2021-06-05 10:14:46 +0700

    compile

--- production/scripts/_mfp.js
+++ production/scripts/_mfp.js
@@ -1,35 +1,24 @@
 $(document).ready(function () {
 
-    var options = {
+    $('.mfp-handler').magnificPopup({
         type: 'inline',     /* to use custom HTML inside of popup */
         removalDelay: 200,
         showCloseBtn: false, /* we will have our own cross button in .window component */
         callbacks: {
             open: function() {
-                /* On popup open re-init Carousels: */
+                /* On popup open re-init Carousels inside: */
                 $('.mfp-content .carousel').each(function () {
                     initCarousel($(this));
                 });
-                /* re-init Expanding Textareas:: */
-                $('.edit-post__message--extendable-via-plugin').expanding();
+                /* re-init Expanding Textareas: */
+                $('.mfp-content .edit-post__message--extendable-via-plugin').expanding();
             }
         }
-    }
+    });
 
-    $('.mfp-handler').magnificPopup(options);
+    $('.mfp-cancel').on('click', function () {
+        $.magnificPopup.close()
+    });
 
-    //
-    // /* Init post-form popup by textarea focus and select click */
-    //
-    // options.items = { src: '#post-form' };
-    //
-    // $('.post-handler__message .input__widget').on('focus', function () {
-    //     $.magnificPopup.open(options);
-    // });
-    //
-    // $('.post-handler__select-author .input__widget').on('click', function (event) {
-    //     event.preventDefault();
-    //     $.magnificPopup.open(options);
-    // });
 
 });
