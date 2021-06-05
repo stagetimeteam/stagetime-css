c45adaa789266980cc1703c79411468adbc9e7ae
Anton Shchukin <a.a.shchukin@gmail.com>
2021-06-05 08:18:55 +0700

    popup init

--- development/scripts/_mfp.js
+++ development/scripts/_mfp.js
@@ -16,4 +16,29 @@ $(document).ready(function () {
         }
     });
 
+
+    // post-handler__select-author
+
+
+    $('.post-handler__message .input__widget').on('focus', function (){
+        $.magnificPopup.open({
+            items: {
+                src: '#post-form',
+            },
+            type: 'inline',
+            removalDelay: 200,
+            showCloseBtn: false, /* we will have our own cross button in .window component */
+            callbacks: {
+                open: function() {
+                    /* On popup open re-init Carousels: */
+                    $('.mfp-content .carousel').each(function () {
+                        initCarousel($(this));
+                    });
+                    /* re-init Expanding Textareas:: */
+                    $('.edit-post__message--extendable-via-plugin').expanding();
+                }
+            }
+        });
+    });
+
 });
