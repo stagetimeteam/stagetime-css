fabf20b8418e65f8d1661d59368f0b984402f42f
Anton Shchukin <a.a.shchukin@gmail.com>
2021-06-05 08:34:45 +0700

    compile

--- production/scripts/_mfp.js
+++ production/scripts/_mfp.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    $('.mfp-handler').magnificPopup({
+    var options = {
         type: 'inline',     /* to use custom HTML inside of popup */
         removalDelay: 200,
         showCloseBtn: false, /* we will have our own cross button in .window component */
@@ -14,6 +14,22 @@ $(document).ready(function () {
                 $('.edit-post__message--extendable-via-plugin').expanding();
             }
         }
-    });
+    }
+
+    $('.mfp-handler').magnificPopup(options);
+
+    //
+    // /* Init post-form popup by textarea focus and select click */
+    //
+    // options.items = { src: '#post-form' };
+    //
+    // $('.post-handler__message .input__widget').on('focus', function () {
+    //     $.magnificPopup.open(options);
+    // });
+    //
+    // $('.post-handler__select-author .input__widget').on('click', function (event) {
+    //     event.preventDefault();
+    //     $.magnificPopup.open(options);
+    // });
 
 });
