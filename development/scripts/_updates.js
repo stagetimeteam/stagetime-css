bd06db56f53c70201a5be3f01bcdb88636124439
Anton Shchukin <a.a.shchukin@gmail.com>
2021-06-09 06:01:25 +0700

    simplify updates switch

--- development/scripts/_updates.js
+++ development/scripts/_updates.js
@@ -2,14 +2,10 @@
 
 
     function updatesSwitch() {
-        if ( $('.updates__switch .toggler__widget').prop('checked') ) {
-            $('.updates').addClass('updates--switched-to-news')
-        } else {
-            $('.updates').removeClass('updates--switched-to-news')
-        }
+        $('.dash-intro .updates').toggleClass('updates--switched-to-news')
+        $('.dash-side .updates').toggleClass('updates--switched-to-news')
     }
 
     $('.updates__switch .toggler__widget').on('change', updatesSwitch);
-    $(document).ready(updatesSwitch);
 
 })(jQuery);
