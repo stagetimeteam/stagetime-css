873aef27716bd68839c1a05fa5073197d6a445a6
Anton Shchukin <a.a.shchukin@gmail.com>
2021-06-09 06:17:28 +0700

    updates script clean up

--- development/scripts/_updates.js
+++ development/scripts/_updates.js
@@ -1,11 +1,7 @@
 (function($) {
 
-
-    function updatesSwitch() {
-        $('.dash-intro .updates').toggleClass('updates--switched-to-news')
-        $('.dash-side .updates').toggleClass('updates--switched-to-news')
-    }
-
-    $('.updates__switch .toggler__widget').on('change', updatesSwitch);
+    $('.updates__switch .toggler__widget').on('change', function (){
+        $('.updates').toggleClass('updates--switched-to-news')
+    });
 
 })(jQuery);
