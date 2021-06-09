0a5f3133864e6f240fff1942f34cdd36e9bc416e
Anton Shchukin <a.a.shchukin@gmail.com>
2021-06-09 08:30:42 +0700

    compile

--- production/scripts/_updates.js
+++ production/scripts/_updates.js
@@ -1,15 +1,7 @@
 (function($) {
 
-
-    function updatesSwitch() {
-        if ( $('.updates__switch .toggler__widget').prop('checked') ) {
-            $('.updates').addClass('updates--switched-to-news')
-        } else {
-            $('.updates').removeClass('updates--switched-to-news')
-        }
-    }
-
-    $('.updates__switch .toggler__widget').on('change', updatesSwitch);
-    $(document).ready(updatesSwitch);
+    $('.updates__switch .toggler__widget').on('change', function (){
+        $('.updates').toggleClass('updates--switched-to-news')
+    });
 
 })(jQuery);
