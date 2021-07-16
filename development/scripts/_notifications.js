bb33b34d3527037eebd2d527fc9660bd3938f0f8
Anton Shchukin <a.a.shchukin@gmail.com>
2021-07-17 01:27:44 +0300

    notifications script

--- development/scripts/_notifications.js
+++ development/scripts/_notifications.js
@@ -1,7 +1,7 @@
 (function($) {
 
     /* desktop handler */
-    $('.header__item:nth-child(3)').on('click', function () {
+    $('.header__item:nth-child(2)').on('click', function () {
         $('html').toggleClass('notifications-visible');
     });
 
@@ -19,7 +19,7 @@
     /* click outside ( goo.gl/SJG2Hw ) */
 
     $(document).on('click', function(event) {
-        if (!$(event.target).closest('.notifications, .header__item:nth-child(3), .header__notifications-handler').length) {
+        if (!$(event.target).closest('.notifications, .header__item:nth-child(2), .header__notifications-handler').length) {
             $('html').removeClass('notifications-visible');
         }
     });
