5171e12a51a369f2ff0a088e0b77241fa884c289
Anton Shchukin <a.a.shchukin@gmail.com>
2021-09-02 16:02:34 +0200

    compile

---
+++ production/scripts/_explore.js
@@ -0,0 +1,15 @@
+(function($) {
+
+    $('.explore__show-search').on('click', function (){
+        $('.explore').addClass('explore--collapsed');
+    });
+
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > 20 ) {
+            $('.explore').addClass('explore--collapsed');
+        } else  {
+            $('.explore').removeClass('explore--collapsed');
+        }
+    });
+
+})(jQuery);
