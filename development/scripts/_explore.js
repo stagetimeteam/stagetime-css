3262ee4c649ba7aa962ac0661d379613598a5743
Anton Shchukin <a.a.shchukin@gmail.com>
2021-09-05 12:14:15 +0200

    dash tiles scripts

--- development/scripts/_explore.js
+++ development/scripts/_explore.js
@@ -1,15 +1,4 @@
 (function($) {
 
-    $('.explore__show-search').on('click', function (){
-        $('html').addClass('explore-collapsed');
-    });
-
-    // $(window).on('scroll', function () {
-    //     if ( $(window).scrollTop() > 20 ) {
-    //         $('.explore').addClass('explore--collapsed');
-    //     } else  {
-    //         $('.explore').removeClass('explore--collapsed');
-    //     }
-    // });
 
 })(jQuery);
