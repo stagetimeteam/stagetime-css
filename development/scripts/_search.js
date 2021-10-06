d0fd87491063873279944d32356d8f1dbdb28043
Anton Shchukin <a.a.shchukin@gmail.com>
2021-10-06 17:32:35 +0200

    header scripts cleanup

--- development/scripts/_search.js
+++ development/scripts/_search.js
@@ -4,11 +4,19 @@
         $('html').addClass('search-visible');
     });
 
+    $('.search-button').on('click', function () {
+        $('html').addClass('search-visible');
+    });
+
+    $('.search__close').on('click', function() {
+        $('html').removeClass('search-visible');
+    });
+
 
     /* hide popup by click outside ( goo.gl/SJG2Hw ) */
 
     $(document).on('click', function(event) {
-        if (!$(event.target).closest('.search, .header__search-handler').length) {
+        if (!$(event.target).closest('.search, .search-button').length) {
             $('html').removeClass('search-visible');
         }
     });
@@ -22,16 +30,6 @@
         }
     });
 
-
-    /* mobile */
-
-    $('.header__search-handler').on('click', function () {
-        $('html').addClass('search-visible');
-    });
-
-    $('.search__close').on('click', function() {
-        $('html').removeClass('search-visible');
-    });
 
 
 
