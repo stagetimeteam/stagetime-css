4acce4fa2646fba73eab305422e7a2ebc5ff7691
Anton Shchukin <a.a.shchukin@gmail.com>
2021-10-12 00:27:17 +0300

    populate flyovers

--- development/scripts/_flyover.js
+++ development/scripts/_flyover.js
@@ -8,9 +8,21 @@
         target = $($(this).attr('data-flyover'));
         peak = target.find('.flyover__peak');
 
+        if( target.hasClass('flyover--organization-feed') ) {
+            target.css({
+                'top': $(this)[0].getBoundingClientRect().top - target.height(),
+                'left': $(this)[0].getBoundingClientRect().left - 37,
+                'transform': 'translateY(-15px)'
+            });
+            peak.css({
+                'bottom': '6px',
+                'left': '60px'
+            })
+        }
+
         if( target.hasClass('flyover--side-card') ) {
             target.css({
-                'top': $(this)[0].getBoundingClientRect().top + $(this).outerHeight(),
+                'top': $(this)[0].getBoundingClientRect().top + $(this).height(),
                 'right': $(window).outerWidth() - $(this)[0].getBoundingClientRect().left - $(this).outerWidth(),
                 'transform': 'translateY(15px)'
             });
