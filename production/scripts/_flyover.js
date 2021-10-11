3109bc6c4fca3348bc4e5c685a7e117c62be1794
Anton Shchukin <a.a.shchukin@gmail.com>
2021-10-12 01:24:25 +0300

    compile

--- production/scripts/_flyover.js
+++ production/scripts/_flyover.js
@@ -1,24 +1,50 @@
 (function($) {
     var target;
-    var top;
-    var right
+    var peak;
+    var peakShadow;
 
     $('[data-flyover]').on('mouseenter', function () {
 
         target = $($(this).attr('data-flyover'));
-        top = $(this)[0].getBoundingClientRect().top + $(this).outerHeight();
-        right = $(window).outerWidth() - $(this)[0].getBoundingClientRect().left - $(this).outerWidth();
+        peak = target.find('.flyover__peak');
 
-        target.css({
-            'display': 'block',
-            'right': right,
-            'top': top
-        });
+        if( target.hasClass('flyover--organization-feed') ) {
+            target.css({
+                'bottom': $(window).outerHeight() - $(this)[0].getBoundingClientRect().top,
+                'left': $(this)[0].getBoundingClientRect().left - 37,
+                'padding-bottom': '15px'
+            });
+            peak.css({
+                'bottom': '9px',
+                'left': '60px'
+            })
+        }
+
+        if( target.hasClass('flyover--side-card') ) {
+            target.css({
+                'top': $(this)[0].getBoundingClientRect().top + $(this).height(),
+                'right': $(window).outerWidth() - $(this)[0].getBoundingClientRect().left - $(this).outerWidth(),
+                'padding-top': '15px'
+            });
+            peak.css({
+                'top': '9px',
+                'right': '46px'
+            })
+        }
+
+        target.addClass('flyover--visible');
 
     }).on('mouseleave', function () {
-        target.css({
-            'display': 'none'
-        });
+        target.removeClass('flyover--visible');
+    });
+
+
+    /* to reload on next hover */
+    $(window).on('scroll', function () {
+        $('.flyover').removeClass('flyover--visible');
+    });
+    $('.scrollbar').on('scroll', function () {
+        $('.flyover').removeClass('flyover--visible');
     });
 
 })(jQuery);
