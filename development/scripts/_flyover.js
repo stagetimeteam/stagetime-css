3c5d46b785c24734e626a53c7abf3e7f2768a368
Anton Shchukin <a.a.shchukin@gmail.com>
2021-10-11 23:28:52 +0300

    rewrite positioning

--- development/scripts/_flyover.js
+++ development/scripts/_flyover.js
@@ -1,19 +1,40 @@
 (function($) {
     var target;
-    var top;
-    var right
+    var peak;
+    var peakShadow;
 
     $('[data-flyover]').on('mouseenter', function () {
 
         target = $($(this).attr('data-flyover'));
-        top = $(this)[0].getBoundingClientRect().top + $(this).outerHeight();
-        right = $(window).outerWidth() - $(this)[0].getBoundingClientRect().left - $(this).outerWidth();
+        peak = target.find('.flyover__peak');
+        peakShadow = target.find('.flyover__peak-shadow');
+
+        if( target.hasClass('flyover--side-card') ) {
+            target.css({
+                'padding-top': '10px',
+                'top': $(this)[0].getBoundingClientRect().top + $(this).outerHeight(),
+                'right': $(window).outerWidth() - $(this)[0].getBoundingClientRect().left - $(this).outerWidth(),
+            });
+            peak.css({
+                'top': '4px',
+                'right': '46px'
+            })
+            peakShadow.css({
+                'top': '4px',
+                'right': '46px'
+            })
+        } else {
+            // top = $(this)[0].getBoundingClientRect().top + $(this).outerHeight();
+            // right = $(window).outerWidth() - $(this)[0].getBoundingClientRect().left - $(this).outerWidth();
+            // bottom = 'auto';
+            // left = 'auto';
+        }
 
         target.css({
-            'display': 'block',
-            'right': right,
-            'top': top
+            'display': 'block'
         });
+
+
 
     }).on('mouseleave', function () {
         target.css({
