311d7327d493297895f9cdbeb3dd9149156eb5f8
Anton Shchukin <a.a.shchukin@gmail.com>
2021-12-13 17:33:49 +0700

    .slideshow

--- development/scripts/_slideshow.js
+++ development/scripts/_slideshow.js
@@ -1,15 +1,54 @@
 (function($) {
 
+
+
+    function slide($slideshow, slideTo) {
+        var $items = $slideshow.find('.slideshow__item')
+        var $itemCurrent = $slideshow.find('.slideshow__item--current');
+        var $counter = $slideshow.find('.slideshow__counter');
+        var $next = $slideshow.find('.slideshow__control--next');
+        var $prev = $slideshow.find('.slideshow__control--prev');
+        var $dots = $slideshow.find('.slideshow__dot');
+        var $dotCurrent = $slideshow.find('.slideshow__dot--current');
+        var current = $itemCurrent.index();
+        var total = $items.length;
+
+        $itemCurrent.removeClass('slideshow__item--current');
+        $slideshow.find('.slideshow__item').eq(slideTo).addClass('slideshow__item--current');
+        $counter.html( (slideTo + 1) + '/' + total )
+
+        $dotCurrent.removeClass('slideshow__dot--current');
+        $slideshow.find('.slideshow__dot').eq(slideTo).addClass('slideshow__dot--current');
+
+
+        /* Disabling arrows */
+
+        $prev.removeClass('slideshow__control--disabled').prop('disabled', false);
+        $next.removeClass('slideshow__control--disabled').prop('disabled', false);
+
+        if ( slideTo === total - 1 ) {
+            $next.addClass('slideshow__control--disabled').prop('disabled', true);
+        } else if ( slideTo === 0 ) {
+            $prev.addClass('slideshow__control--disabled').prop('disabled', true);
+        }
+
+    }
+
+
+    /* Navigation using dots */
+
+    $('.slideshow__dot').on('click', function () {
+        slide( $(this).parents('.slideshow'), $(this).index());
+    });
+
+
+    /* Navigation using arrows */
+
     $('.slideshow__control').on('click', function () {
         var $this = $(this);
         var $slideshow = $this.parents('.slideshow');
-        var $items = $slideshow.find('.slideshow__item')
-        var $current = $slideshow.find('.slideshow__item--current');
-        var $counter = $slideshow.find('.slideshow__counter');
-        var $slideshowNext = $slideshow.find('.slideshow__control--next');
-        var $slideshowPrev = $slideshow.find('.slideshow__control--prev');
-        var current = $current.index();
-        var total = $items.length;
+        var current = $slideshow.find('.slideshow__item--current').index();
+        var total = $slideshow.find('.slideshow__item').length;
         var slideTo = 0;
 
 
@@ -31,21 +70,7 @@
             }
         }
 
-        $current.removeClass('slideshow__item--current');
-        $slideshow.find('.slideshow__item').eq(slideTo).addClass('slideshow__item--current');
-        $counter.html( (slideTo + 1) + '/' + total )
-
-
-        /* Disabling arrows */
-
-        $slideshowPrev.removeClass('slideshow__control--disabled').prop('disabled', false);
-        $slideshowNext.removeClass('slideshow__control--disabled').prop('disabled', false);
-
-        if ( slideTo === total - 1 ) {
-            $slideshowNext.addClass('slideshow__control--disabled').prop('disabled', true);
-        } else if ( slideTo === 0 ) {
-            $slideshowPrev.addClass('slideshow__control--disabled').prop('disabled', true);
-        }
+        slide( $(this).parents('.slideshow'), slideTo);
 
     });
 
