f0f204ff7d5d6e8914fdfdbece3b77871384654d
Anton Shchukin <a.a.shchukin@gmail.com>
2021-12-29 08:23:54 +0700

    globalize parallax

--- development/scripts/_parallax-photo.js
+++ development/scripts/_parallax-photo.js
@@ -1,19 +1,28 @@
 (function($) {
 
-    var $container = $('.parallax-photo');
-    var $image = $('.parallax-photo__image');
-    var height = $container.outerHeight();
-    var offset = $container.offset().top - $('.header').outerHeight();
+    var data = [];
+    var headerHeight = $('.header').outerHeight();
     var shift = 150; /* 150px -- must be same to height value in .parallax-photo__image */
     var scrolled;
     var newValue;
     var normalizedValue;
 
+    $('.parallax-photo').each(function (i) {
+        data.push({
+            '$container': $(this),
+            '$image': $(this).find('.parallax-photo__image'),
+            'height': $(this).outerHeight(),
+            'offset': $(this).offset().top - headerHeight,
+        });
+    });
+
     function parallax() {
-        scrolled = $(this).scrollTop() - offset;
-        newValue = scrolled / height * shift;
-        normalizedValue = Math.max( 0, Math.min(shift, newValue ) ); /* value in between [0, shift] */
-        $image.css('transform', 'translateY(-' + normalizedValue + 'px)')
+        data.forEach(function (current) {
+            scrolled = $(this).scrollTop() - current.offset;
+            newValue = scrolled / current.height * shift;
+            normalizedValue = Math.max( 0, Math.min(shift, newValue ) ); /* value in between [0, shift] */
+            current.$image.css('transform', 'translateY(-' + normalizedValue + 'px)')
+        });
     }
 
     $(window).on('scroll', parallax);
