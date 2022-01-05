1c3f3c49ba1b1f39fa30243273c9bfeec4f8014b
Anton Shchukin <a.a.shchukin@gmail.com>
2022-01-05 12:03:25 +0700

    anchor doc

--- development/scripts/_anchor.js
+++ development/scripts/_anchor.js
@@ -1,12 +1,14 @@
 (function($) {
 
+
     var scrollSpeedPer100ms = 200; /* 200px per 100ms */
 
     $('.anchor').on('click', function (event) {
         event.preventDefault();
 
+
         var $target = $( $(this).attr('href') );
-        var headerHeight = $('.header').outerHeight();
+        var headerHeight = $('.header').length ? $('.header').outerHeight() : 0;
         var scrollDistance = $target.offset().top - $(this).offset().top - headerHeight;
         var animationDuration = Math.max(Math.abs(scrollDistance) / scrollSpeedPer100ms * 100, 300); /* Not less than 300ms (for short distance) */
         var scrollGap = $(this).attr('data-anchor-gap') ? +$(this).attr('data-anchor-gap') : 0;
