c7aa78ac9d92b6d77961a56a24d4692bc85a1393
Anton Shchukin <a.a.shchukin@gmail.com>
2022-01-21 09:36:49 +0700

    comment

--- development/scripts/_modal.js
+++ development/scripts/_modal.js
@@ -1,7 +1,6 @@
 (function($) {
 
-
-
+    /* Remove disabling class from submit button */
     $('.modal').on('click', function () {
         $(this).find('.modal-foot__operations').find('.button--disabled').removeClass('button--disabled')
     });
