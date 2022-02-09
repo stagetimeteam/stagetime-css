42099cf8d551799c497119a18752e5469bda2b56
Anton Shchukin <a.a.shchukin@gmail.com>
2022-02-09 21:19:26 +0700

    compile

--- production/scripts/_color-selection.js
+++ production/scripts/_color-selection.js
@@ -1,9 +1,33 @@
 (function($) {
 
-    $('.color-selection__item').on('click', function (){
-        $(":root").css("--user-color", $(this).text());
+    var themesData = {};
+
+    $.ajax({
+        url: '../themes/themes.json',
+        dataType: 'json',
+        async: false,
+        success: function(data) {
+            themesData = { ...data };
+        }
+    });
+
+
+    function changeTheme(newTheme) {
         $('.color-selection__item--current').removeClass('color-selection__item--current');
-        $(this).addClass('color-selection__item--current');
+        $('[data-theme="' + newTheme + '"]').addClass('color-selection__item--current');
+
+        $.each(themesData[newTheme], function( key, value ) {
+            $(":root").css("--" + key, value);
+        });
+    }
+
+
+    $(document).ready(function () {
+        changeTheme($('.color-selection__item--current').data('theme'));
+    });
+
+    $('.color-selection__item').on('click', function () {
+        changeTheme($(this).data('theme'));
     });
 
 })(jQuery);
