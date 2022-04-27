3e57780f1ce997b6e9fd3f9e785290369a5fcd04
Anton Shchukin <a.a.shchukin@gmail.com>
2022-04-27 09:35:40 +0700

    crew layout draft

--- development/scripts/_crew.js
+++ development/scripts/_crew.js
@@ -3,12 +3,12 @@
     $('.crew__tag').on('click', function () {
         if( ! $(this).hasClass('crew__tag--current') ) {
 
-            $('.crew__panel--visible').removeClass('crew__panel--visible');
+            $('.crew__body--visible').removeClass('crew__body--visible');
 
-            if( $(this).hasClass('crew__tag--chat'     ) ) { $('.crew__panel--chat'     ).addClass('crew__panel--visible'); }
-            if( $(this).hasClass('crew__tag--workspace') ) { $('.crew__panel--workspace').addClass('crew__panel--visible'); }
-            if( $(this).hasClass('crew__tag--members'  ) ) { $('.crew__panel--members'  ).addClass('crew__panel--visible'); }
-            if( $(this).hasClass('crew__tag--resources') ) { $('.crew__panel--resources').addClass('crew__panel--visible'); }
+            if( $(this).hasClass('crew__tag--chat'     ) ) { $('.crew__body--chat'     ).addClass('crew__body--visible'); }
+            if( $(this).hasClass('crew__tag--workspace') ) { $('.crew__body--workspace').addClass('crew__body--visible'); }
+            if( $(this).hasClass('crew__tag--members'  ) ) { $('.crew__body--members'  ).addClass('crew__body--visible'); }
+            if( $(this).hasClass('crew__tag--resources') ) { $('.crew__body--resources').addClass('crew__body--visible'); }
 
             $('.crew__tag--current').removeClass('crew__tag--current');
             $(this).addClass('crew__tag--current');
