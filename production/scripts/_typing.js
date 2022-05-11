0f2127b816dcc79759a91f88c767d8dca896b335
Anton Shchukin <a.a.shchukin@gmail.com>
2022-05-11 12:38:17 +0700

    compile

--- production/scripts/_typing.js
+++ production/scripts/_typing.js
@@ -1,11 +1,6 @@
 (function($) {
 
-    $('.typing__message').each(function () {
-        this.setAttribute('style', 'height:' + (this.scrollHeight) + 'px;overflow-y: hidden;');
-    }).on('input', function () {
-        this.style.height = 'auto';
-        this.style.height = (this.scrollHeight) + 'px';
-    });
+    $('.typing__message').expanding();
 
     $('.typing__message').on('keypress', function () {
         $('.typing__send').removeClass('typing__send--disabled');
