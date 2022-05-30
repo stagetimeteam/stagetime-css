710ca00e19b464f231aa96e2389bebec03c021fc
Anton Shchukin <a.a.shchukin@gmail.com>
2022-05-30 16:48:54 +0700

    owl draft 2

--- development/scripts/_owl-carousel.js
+++ development/scripts/_owl-carousel.js
@@ -1,23 +1,13 @@
 (function($) {
 
-    $(".owl-carousel").owlCarousel({
-        nav: true,
-        responsive:{
-            0:{
-                items:1,
-                nav:true
-            },
-            600:{
-                items:3,
-                nav:false
-            },
-            1000:{
-                items:5,
-                nav:true,
-                loop:false
-            }
-        }
-    });
+    if(desktopMediaQuery.matches) {
+        $('.owl-carousel--profile-media').owlCarousel({
+            items: 5,
+            nav: true,
+            loop: false
+        });
+    }
+
 
 
 
