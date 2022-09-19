4077a6a2a132585991c4503ec14f9b120e00db3f
Anton Shchukin <a.a.shchukin@gmail.com>
2022-09-20 01:54:41 +0700

    re-compile

---
+++ production/scripts/_full-biography.js
@@ -0,0 +1,7 @@
+(function($) {
+
+    $('.full-biography__expand').on('click', function () {
+        $(this).parents('.full-biography').addClass('full-biography--expanded');
+    });
+
+})(jQuery);
--- production/scripts/_owl-carousel.js
+++ production/scripts/_owl-carousel.js
@@ -1,31 +1,57 @@
-(function($) {
+(function ($) {
 
-    var owls;
+    if (desktopMediaQuery.matches) {
 
-    if(desktopMediaQuery.matches) {
-        owls = $('.owl-carousel--profile-media').owlCarousel({
-            items: 3,
-            slideBy: 2,
-            margin: 44,
-            nav: true,
-            dots: false,
-            loop: false
-        });
-    }
-
-    if(desktopMediaQuery.matches) {
-        owls = $('.owl-carousel--classic-projects').owlCarousel({
-            items: 3,
+        /* Performer profile media */
+        var owlProjectsMedia = $('.owl-carousel--profile-media').owlCarousel({
+            items: 4,
             slideBy: 3,
             margin: 56,
             nav: true,
             dots: false,
             loop: false
         });
+
+        owlProjectsMedia.each(function () {
+            $(this).on('mousewheel', '.owl-stage', function (e) {
+                if (e.deltaX > 0) {
+                    $(this).trigger('next.owl');
+                } else {
+                    $(this).trigger('prev.owl');
+                }
+                e.preventDefault();
+            });
+        });
+
+
+
+        /* Performer profile media */
+        var owlFullProjectsMedia = $('.owl-carousel--full-profile-media').owlCarousel({
+            items: 3,
+            slideBy: 2,
+            margin: 36,
+            nav: true,
+            dots: false,
+            loop: false
+        });
+
+        owlFullProjectsMedia.each(function () {
+            $(this).on('mousewheel', '.owl-stage', function (e) {
+                if (e.deltaX > 0) {
+                    $(this).trigger('next.owl');
+                } else {
+                    $(this).trigger('prev.owl');
+                }
+                e.preventDefault();
+            });
+        });
+
+
     }
 
 
-    var owlProfileProjects = $('.owl-carousel--profile-projects').owlCarousel({
+
+    $('.owl-carousel--profile-projects').owlCarousel({
         slideBy: 1,
         smartSpeed: 500,
         dots: false,
@@ -44,20 +70,6 @@
             }
         }
     });
-
-    owls.push(owlProfileProjects);
-
-    owls.each(function () {
-        $(this).on('mousewheel', '.owl-stage', function (e) {
-            if (e.deltaX > 0) {
-                $(this).trigger('next.owl');
-            } else {
-                $(this).trigger('prev.owl');
-            }
-            e.preventDefault();
-        });
-    })
-
 
 
 })(jQuery);
