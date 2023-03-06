b1b8cc4f62deccaa67046f01a901f90f4b8c17dd
Anton Shchukin <a.a.shchukin@gmail.com>
2023-03-07 03:36:21 +0700

    compile

--- production/scripts/_spotlight.js
+++ production/scripts/_spotlight.js
@@ -1,21 +1,18 @@
 (function($) {
 
     function spotlightShow(spotlight, underspotlight) {
-        spotlight.fadeIn(150);
+        spotlight.addClass('spotlight--current');
         underspotlight.addClass('under-the-spotlight');
     }
 
-    function spotlightHide(){
-        $('.spotlight').fadeOut(150, function () {
-            $('.under-the-spotlight').removeClass('under-the-spotlight'); /* remove .under-the-spotlight only when animation is over */
-        });
-
+    function spotlightHide() {
+        $('.spotlight').removeClass('spotlight--current');
+        $('.under-the-spotlight').removeClass('under-the-spotlight');
     }
 
 
     $('[data-spotlight]').on('click', function(event) {
         event.preventDefault();
-        console.log( $($(this).data('spotlight')), $($(this).data('underspotlight')) );
         spotlightShow( $($(this).data('spotlight')), $($(this).data('underspotlight')) );
     });
 
