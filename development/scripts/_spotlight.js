467784ace3aa6392b0a392082a536406a293a178
Anton Shchukin <a.a.shchukin@gmail.com>
2023-03-06 22:22:41 +0700

    spotlight

---
+++ development/scripts/_spotlight.js
@@ -0,0 +1,34 @@
+(function($) {
+
+    function spotlightShow(spotlight){
+        spotlight.fadeIn(150);
+    }
+
+    function spotlightHide(){
+        $('.spotlight').fadeOut();
+    }
+
+
+    $('[data-spotlight]').on('click', function(event) {
+        event.preventDefault();
+        spotlightShow( $($(this).data('spotlight')) );
+    });
+
+    $('.spotlight__close').on('click', function(event) {
+        event.preventDefault();
+        spotlightHide();
+    });
+
+    $('.spotlight__curtain').on('click', function(event) {
+        event.preventDefault();
+        spotlightHide();
+    });
+
+
+    $(document).on('keyup', function(event) {
+        if (event.keyCode === 27) {
+            spotlightHide();
+        }
+    });
+
+})(jQuery);
